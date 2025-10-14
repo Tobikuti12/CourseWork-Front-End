c69a934ce27c9c572bbe81bb92fd6a2a7181236d
Tobi Kuti <tobikuti12@gmail.com>
2025-10-14 16:51:20 +0400

    First Files

---
+++ AfterSchoolLessons.js
@@ -0,0 +1,100 @@
+let products = [
+            {
+                    "id": 1002,
+                    "title": "Swimming",
+                    "price":2000,
+                     "image": "image/swimming.webp",
+                    "availableInventory" : 5,
+                    "location":"B15-2-18",
+                    "rating" : 2
+            },
+            {
+                    "id": 1003,
+                    "title": "Football",
+                    "price":2001,
+                    "image": "image/Football.webp",
+                    "availableInventory": 5,
+                    "location":"B16-1-18",
+                    "rating" : 4
+            
+            },
+        {
+                   "id" : 1004,
+                    "title": "Karate",
+                    "price":3000,
+                    "image": "image/Karate.webp",
+                    "availableInventory": 5,
+                    "location":"B17-2-18",
+                    "rating" : 3 
+        },
+              {
+                  "id" : 1005,
+                    "title": "Drawing",
+                    "price":4000,
+                    "image": "image/Drawing.webp",
+                    "availableInventory": 5,
+                    "location":"B19-1-18",
+                    "rating" : 3 
+              },
+                 {  
+                    "id" : 1006,
+                    "title": "Acting",
+                    "price":1500,
+                    "image": "image/Acting.webp",
+                    "availableInventory": 5,
+                    "location":"B15-1-19",
+                    "rating" : 3 
+                    }
+,
+                 {
+                     "id" : 1007,
+                    "title": "Cooking",
+                    "price":1345,
+                    "image": "image/Cooking.webp",
+                    "availableInventory": 5,
+                    "location":"B16-2-18",
+                    "rating" : 3 
+
+                 } 
+                 ,
+                 {
+                    "id" : 1008,
+                    "title": "Fashion",
+                    "price":1140,
+                    "image": "image/Fashion.webp",
+                    "availableInventory": 5,
+                    "location":"B15-3-18",
+                    "rating" : 3 
+                 }
+                ,
+                  {"id" : 1009,
+                    "title": "Tennis",
+                    "price":1500,
+                    "image": "image/Tennis.webp",
+                    "availableInventory": 5,
+                    "location":"B15-4-18",
+                    "rating" : 3 
+                  }
+                  ,
+                  {
+                    "id" : 1010,
+                    "title": "Track and Field",
+                    "price":1087,
+                    "image": "image/Track and Field.webp",
+                    "availableInventory": 5,
+                    "location":"B15-6-18",
+                    "rating" : 3 
+                  }
+                  ,
+                  {
+                     "id" : 1011,
+                    "title": "Poetry",
+                    "price":1500,
+                    "image": "image/Poetry.webp",
+                    "availableInventory": 5,
+                    "location":"B8-1-18",
+                    "rating" : 3 
+                  }
+            
+
+    ]
